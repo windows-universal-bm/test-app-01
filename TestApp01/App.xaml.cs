1cfc2c98cc80d62939436625d2757c56b5d2ab47
bigmogician <bigmogician@outlook.com>
2020-04-14 14:16:47 +0800

    feat: xbox login completed

--- TestApp01/App.xaml.cs
+++ TestApp01/App.xaml.cs
@@ -15,19 +15,16 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-namespace TestApp01
-{
+namespace TestApp01 {
     /// <summary>
     /// 提供特定于应用程序的行为，以补充默认的应用程序类。
     /// </summary>
-    sealed partial class App : Application
-    {
+    sealed partial class App : Application {
         /// <summary>
         /// 初始化单一实例应用程序对象。这是执行的创作代码的第一行，
         /// 已执行，逻辑上等同于 main() 或 WinMain()。
         /// </summary>
-        public App()
-        {
+        public App() {
             this.InitializeComponent();
             this.Suspending += OnSuspending;
         }
@@ -37,21 +34,18 @@ namespace TestApp01
         /// 将在启动应用程序以打开特定文件等情况下使用。
         /// </summary>
         /// <param name="e">有关启动请求和过程的详细信息。</param>
-        protected override void OnLaunched(LaunchActivatedEventArgs e)
-        {
+        protected override void OnLaunched(LaunchActivatedEventArgs e) {
             Frame rootFrame = Window.Current.Content as Frame;
 
             // 不要在窗口已包含内容时重复应用程序初始化，
             // 只需确保窗口处于活动状态
-            if (rootFrame == null)
-            {
+            if (rootFrame == null) {
                 // 创建要充当导航上下文的框架，并导航到第一页
                 rootFrame = new Frame();
 
                 rootFrame.NavigationFailed += OnNavigationFailed;
 
-                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
-                {
+                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated) {
                     //TODO: 从之前挂起的应用程序加载状态
                 }
 
@@ -59,10 +53,8 @@ namespace TestApp01
                 Window.Current.Content = rootFrame;
             }
 
-            if (e.PrelaunchActivated == false)
-            {
-                if (rootFrame.Content == null)
-                {
+            if (e.PrelaunchActivated == false) {
+                if (rootFrame.Content == null) {
                     // 当导航堆栈尚未还原时，导航到第一页，
                     // 并通过将所需信息作为导航参数传入来配置
                     // 参数
@@ -78,8 +70,7 @@ namespace TestApp01
         /// </summary>
         ///<param name="sender">导航失败的框架</param>
         ///<param name="e">有关导航失败的详细信息</param>
-        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
-        {
+        void OnNavigationFailed(object sender, NavigationFailedEventArgs e) {
             throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
         }
 
@@ -90,8 +81,7 @@ namespace TestApp01
         /// </summary>
         /// <param name="sender">挂起的请求的源。</param>
         /// <param name="e">有关挂起请求的详细信息。</param>
-        private void OnSuspending(object sender, SuspendingEventArgs e)
-        {
+        private void OnSuspending(object sender, SuspendingEventArgs e) {
             var deferral = e.SuspendingOperation.GetDeferral();
             //TODO: 保存应用程序状态并停止任何后台活动
             deferral.Complete();
--- TestApp01/MainPage.xaml.cs
+++ TestApp01/MainPage.xaml.cs
@@ -12,19 +12,89 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Microsoft.Xbox.Services.System;
+using Windows.ApplicationModel.Core;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
-// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
-
-namespace TestApp01
-{
+namespace TestApp01 {
     /// <summary>
     /// 可用于自身或导航至 Frame 内部的空白页。
     /// </summary>
-    public sealed partial class MainPage : Page
-    {
-        public MainPage()
-        {
-            this.InitializeComponent();
+    public sealed partial class MainPage : Page, INotifyPropertyChanged {
+
+        private string logined = "false";
+        public string Logined {
+            get { return logined; }
+            set {
+                logined = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string loginCancel = "false";
+        public string LoginCancel {
+            get { return loginCancel; }
+            set {
+                loginCancel = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string needAuth = "false";
+        public string NeedAuth {
+            get { return needAuth; }
+            set {
+                needAuth = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainPage() {
+            InitializeComponent();
+            OpenXboxLogin();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public async void OpenXboxLogin() {
+            Debug.WriteLine("func start");
+            var xboxUser = new XboxLiveUser();
+            Debug.WriteLine("use xbox user");
+            var dispatcher = CoreApplication.GetCurrentView().CoreWindow.Dispatcher;
+            Debug.WriteLine("use core dispatcher");
+            var result = await xboxUser.SignInSilentlyAsync(dispatcher);
+            Debug.WriteLine("check xbox login result");
+            await CheckResult(xboxUser, result);
+            Debug.WriteLine("func exit");
+            Debug.WriteLine("Logined --> " + Logined);
+            Debug.WriteLine("Cancel --> " + LoginCancel);
+            Debug.WriteLine("NeedAuth --> " + NeedAuth);
+            return;
+        }
+
+        private async Task CheckResult(XboxLiveUser xbox, SignInResult result) {
+            if (result.Status == SignInStatus.Success) {
+                Logined = "true";
+                Debug.WriteLine("logined");
+                return;
+            }
+            if (result.Status == SignInStatus.UserCancel) {
+                LoginCancel = "true";
+                Debug.WriteLine("loginCancel");
+                return;
+            }
+            NeedAuth = "true";
+            Debug.WriteLine("needAuth");
+            var dispatcher = CoreApplication.GetCurrentView().CoreWindow.Dispatcher;
+            var result2 = await xbox.SignInAsync(dispatcher);
+            await CheckResult(xbox, result2);
         }
     }
 }
